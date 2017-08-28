d0e89fee67d69441671735b2d8916e242a2eb1f3
Haoran Feng <haoran.feng@alo7.com>
2017-08-28 13:26:13 +0800

    ok try

--- 01-utils/yml/httpd/download_rpm.sh
+++ 01-utils/yml/httpd/download_rpm.sh
@@ -1,12 +1,14 @@
 # this file should be run only once if the rpm files in version control system are outdated
-sudo yum install -y creatrepo yum-utils device-mapper-persistent-data lvm2
+sudo yum install -y creatrepo yum-utils
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-yumdownloader --resolve --destdir=./localrpm ca-certificates \
+yumdownloader --resolve --archlist=x86_64 --destdir=./localrpm ca-certificates \
+              device-mapper-persistent-data \
               docker-ce \
               gcc \
               libffi-devel \
+              lvm2 \
               openssl \
               python-devel \
               python-pip \
