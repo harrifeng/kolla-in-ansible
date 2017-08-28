9be41537b63d2818e6cae4d1599a169e11a97ba9
Haoran Feng <haoran.feng@alo7.com>
2017-08-28 16:39:23 +0800

    cirros website is always failing

--- 01-utils/httpd/download_large_file.sh
+++ 01-utils/httpd/download_large_file.sh
@@ -10,5 +10,5 @@ function download_if_not_here {
 download_if_not_here '.' 'centos-source-registry-ocata.tar.gz' 'http://tarballs.openstack.org/kolla/images'
 
 # cloud image
-download_if_not_here '.' 'cirros-0.3.4-x86_64-disk.img' 'http://download.cirros-cloud.net/0.3.4'
+# download_if_not_here '.' 'cirros-0.3.4-x86_64-disk.img' 'http://download.cirros-cloud.net/0.3.4'
 download_if_not_here '.' 'xenial-server-cloudimg-amd64-disk1.img' 'http://cloud-images.ubuntu.com/xenial/current'
