be1a69bff8a3cfc0ac5852a6bb638a9423aa4c1e
Harri Feng <harrifeng@gmail.com>
2017-08-28 16:48:11 +0800

    Dev2 (#1)
    All in one deploy can work normally

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -3,7 +3,7 @@ sudo yum install -y epel-release
 sudo yum install -y yum-utils createrepo
 sudo yum install -y python-devel python-pip gcc
 
-createrepo  /vagrant/yml/httpd/localrpm
+createrepo  /vagrant/httpd/localrpm
 echo "pip----------->"
 sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
