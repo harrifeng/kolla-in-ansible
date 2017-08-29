d25fcc159cdb4fe8f197d9c5c65652273f63a665
Haoran Feng <haoran.feng@alo7.com>
2017-08-29 09:46:10 +0800

    new names

--- 03-mutinode/deploy_new_aio_2.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# run on deployment-1
-kolla-ansible post-deploy
-source /etc/kolla/admin-openrc.sh
-/usr/share/kolla-ansible/init-runonce
--- 03-mutinode/deployment_1_new_multinode.sh
+++ 03-mutinode/deployment_1_new_multinode.sh
@@ -4,3 +4,6 @@
 /usr/share/kolla-ansible/tools/cleanup-containers
 
 kolla-ansible deploy -i /root/inventory/multinode
+kolla-ansible post-deploy -i /root/inventory/multinode
+source /etc/kolla/admin-openrc.sh
+/usr/share/kolla-ansible/init-runonce
