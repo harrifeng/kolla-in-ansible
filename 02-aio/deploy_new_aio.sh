f43b91960667693c374b37c62472a8933a237173
Harri Feng <harrifeng@gmail.com>
2017-08-29 19:14:26 +0800

    to be able to run on 32-GB machine

--- 02-aio/deploy_new_aio.sh
+++ 02-aio/deploy_new_aio.sh
@@ -1,10 +1,4 @@
 #! /bin/bash
-# first to clean up all the container
-docker stop `docker ps -a -q`
-docker system prune --force
-/usr/share/kolla-ansible/tools/cleanup-host
-/usr/share/kolla-ansible/tools/cleanup-containers
-
 kolla-ansible deploy -i /root/inventory/all-in-one
 ip addr add 10.0.24.254/24 dev br-ex
 ip link set br-ex up
