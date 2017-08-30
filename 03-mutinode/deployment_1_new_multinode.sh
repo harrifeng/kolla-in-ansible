f5067d09f16ece718ba46752086f976e5c0ff5fc
Haoran Feng <haoran.feng@alo7.com>
2017-08-30 15:49:36 +0800

    delete these files

--- 03-mutinode/deployment_1_new_multinode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# run on deployment-1
-/usr/share/kolla-ansible/tools/cleanup-host
-/usr/share/kolla-ansible/tools/cleanup-containers
-
-kolla-ansible deploy -i /root/inventory/multinode
-kolla-ansible post-deploy -i /root/inventory/multinode
-source /etc/kolla/admin-openrc.sh
-/usr/share/kolla-ansible/init-runonce
--- 03-mutinode/network_1_setup_network.sh
+++
@@ -1,3 +0,0 @@
-# run on network-1
-ip addr add 10.0.26.254/24 dev br-ex
-ip link set br-ex up
