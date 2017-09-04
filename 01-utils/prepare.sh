62e3b0474750decd238fbcccabf898fe4e956aac
Harri Feng <harrifeng@gmail.com>
2017-09-04 11:02:27 +0800

    Mini (#6)
    minimize the project

--- 01-utils/prepare.sh
+++ 01-utils/prepare.sh
@@ -1,11 +1,10 @@
 chmod 600 /home/vagrant/.ssh/id_rsa
+
 sudo yum install -y epel-release
-sudo yum install -y yum-utils createrepo
-sudo yum install -y python-devel python-pip gcc
+sudo yum install -y yum-utils createrepo python-devel python-pip gcc
 
 createrepo  /vagrant/httpd/localrpm
-echo "pip----------->"
+
 sudo pip install -U pip -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
 sudo pip install ansible -i http://mirrors.aliyun.com/pypi/simple --trusted-host=mirrors.aliyun.com
-sudo cp /vagrant/yml/tasks/config/ansible.cfg /vagrant/yml/ansible.cfg
-echo "finishing provision--------------->"
+sudo cp /vagrant/yml/tasks/config/ansible.cfg /etc/ansible/ansible.cfg
